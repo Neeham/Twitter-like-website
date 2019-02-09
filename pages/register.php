fbd44eb3adb87ad93d237c850466097c0971effa
Neeham Khalid <Neehamk@gmail.com>
2019-02-08 21:47:47 -0500

    Forgot to add login page to return back in case the user did not want to register an account.

--- pages/register.php
+++ pages/register.php
@@ -17,10 +17,11 @@ echo alert('error', 'The username already exists. Please choose another username
       <input type="text" class="form-control" name="username" placeholder="<?php echo _("User Name")?>" required=""/><br>
       <input type="password" class="form-control" name="password" placeholder="<?php echo _("Password")?>" required=""/><br>
       <input type="text" class="form-control" name="email" placeholder="<?php echo _("Email Address")?>" required=""/><br><br>
-      <button class="btn btn-lg btn-primary btn-block" name="register" type="submit">
+      <button class="btn btn-lg btn-warning btn-block" name="register" type="submit">
         <?php echo _("Register")?>
       </button><br>
     </form>
+    <button onclick="location.href = 'https://haxstar.com/';" class="btn btn-lg btn-primary btn-block" name="login"><?php echo _("Login")?></button><br>
     <?php include '../repeated/footer.php';?>
   </body>
 </html>
