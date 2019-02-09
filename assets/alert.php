55d5c19bc8dc008fd8b01e22ab3c79e4d0eba5fb
Neeham Khalid <Neehamk@gmail.com>
2019-02-08 21:44:15 -0500

    Changed the close button color of the alert popup box.

--- assets/alert.php
+++ assets/alert.php
@@ -1,7 +1,7 @@
 <?php
-	  function alert($type, $msg){
-	  if ($type == "error") { ?>
-      <style>
+function alert($type, $msg){
+if ($type == "error") { ?>
+<style>
 .modal-header {
 background-color: #FF0000;
 }
@@ -18,15 +18,12 @@ background-color: #00CF37;
 </style>
   <div class="modal" tabindex="-1" id="myModal" role="dialog">
     <div class="modal-dialog" role="document">
-
-      <!-- Modal content-->
       <div class="modal-content">
         <div class="modal-header">
-
           <?php if ($type == "error") { ?>
           <h4 class="modal-title"><?php echo _("Error")?></h4>
           <?php } else if ($type == "success") {?>
-		  <h4 class="modal-title"><?php echo _("Success")?></h4>
+		  			<h4 class="modal-title"><?php echo _("Success")?></h4>
           <?php } ?>
 					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
 						<span aria-hidden="true">&times;</span>
@@ -36,13 +33,11 @@ background-color: #00CF37;
           <p><?php echo $msg ?></p>
         </div>
         <div class="modal-footer">
-          <button type="button" class="btn btn-default" data-dismiss="modal"><?php echo _("Close")?></button>
+          <button type="button" class="btn btn-primary" data-dismiss="modal"><?php echo _("Close")?></button>
         </div>
       </div>
-
     </div>
   </div>
-
 
   <script type=text/javascript>
 $('#myModal').modal('show');
