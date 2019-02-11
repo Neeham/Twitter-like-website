1260cb5a706ce3425e239f896f5b617592fa3f7b
Hassan Sarhan <hasoun7772@gmail.com>
2019-02-11 03:25:53 -0500

    Added placeholder delete button to newly created tweets

--- js/feed.js
+++ js/feed.js
@@ -3,6 +3,8 @@ const quackBox = document.getElementById("quack-box");
 const quackLimit = document.getElementById("quack-limit");
 const quackButton = document.getElementById("quack-button");
 const quackList = document.getElementById("quack-list");
+const deleteButton = document.querySelector(".delete-button");
+
 //Function to count the number of characters in the text field
 function setCounter() {
 
@@ -14,18 +16,20 @@ function setCounter() {
 //Runs the createQuack() method when the Quack button is pressed
 quackButton.addEventListener("click", createQuack);
 
+
+
 //Function to post a new Quack
 function createQuack() {
 
 
-  //Create a new list item to add to list of quacks
+  //Create a new list item to add to the feed
   var li = document.createElement("li");
   li.setAttribute('class', "list-group-item quack");
   li.innerHTML = `
     <div class="media">
       <img src="https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png" alt="" />
       <div class="media-body mx-2">
-        <h5><a href="#">@YOUR_NAME</a></h5>
+        <h5><a href="#">@YOUR_NAME</a><span class="float-right"><button class = "btn btn-danger delete-button"><i class="fas fa-times"></i> Delete</button></span></h5>
         ${quackBox.value} <br />
         <button class="btn float-right btn-danger like mx-1">
           <i class="fas fa-heart"></i>
@@ -37,6 +41,7 @@ function createQuack() {
   setCounter();
 }
 
+// Emoji picker setup
 $(document).ready(function (e) {
 
   $('.emoji-field').emojiPicker({
