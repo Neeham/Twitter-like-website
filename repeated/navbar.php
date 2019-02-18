5b84bb2c695e63d344c8319d92a6a22290deb436
Hassan Sarhan <hasoun7772@gmail.com>
2019-02-17 21:07:36 -0500

    Rearranged the navbar

--- repeated/navbar.php
+++ repeated/navbar.php
@@ -3,23 +3,29 @@ require $_SERVER['DOCUMENT_ROOT'] . '/assets/loggedin.php';
 session_start();
 ?>
 <nav class="navbar navbar-expand-sm bg-warning navbar-light">
-  <a class="navbar-brand" href="https://www.haxstar.com/pages/feed"><img src="https://haxstar.com/images/logo/duck.png"
-      height="35px" /></a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
   <div class="container">
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="https://www.haxstar.com/pages/profile">My Profile</a>
-        </li>
-        <li class="nav-item float-right">
-          <a class="nav-link" href="https://www.haxstar.com/assets/logout">Log out</a>
-        </li>
+    <a class="navbar-brand" href="https://www.haxstar.com/pages/feed"><img
+        src="https://haxstar.com/images/logo/duck.png" height="35px" /></a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.haxstar.com/pages/profile">My Profile</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.haxstar.com/pages/feed">Feed</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="https://www.haxstar.com/assets/logout">Log out</a>
+      </li>
 
-      </ul>
-    </div>
+
+    </ul>
+
+    </ul>
+  </div>
+  </div>
   </div>
 </nav>
