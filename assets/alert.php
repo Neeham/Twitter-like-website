7b56b331ce9248aed0bf821d3b1868efc9b2b98d
Neeham Khalid <Neehamk@gmail.com>
2019-02-20 13:44:11 -0500

    Rebuild URL once the Alert is closed to remove the Alert paramter.

--- assets/alert.php
+++ assets/alert.php
@@ -1,5 +1,9 @@
 <?php
 function alert($type, $title ,$msg){
+
+$currentURL = 'https://'. $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']; //The current URL of the page
+$modifyURL = modify_url($currentURL, 'Alert'); //Modify the URL to remote the Alert parameter (This URL will be called when Alert is closed)
+
 if ($type == "error") { ?>
 
 <style>
@@ -38,7 +42,7 @@ background-color: #ffa500;
             <?php } else if ($type == "warning") {?>
             <h4 class="modal-title"><?php echo $title?></h4>
             <?php } ?>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick="location.href=window.location.pathname">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close" onClick="location.href='<?PHP echo $modifyURL; ?>'">
             <span aria-hidden="true">&times;</span>
             </button>
          </div>
@@ -46,7 +50,7 @@ background-color: #ffa500;
             <p><?php echo $msg ?></p>
          </div>
          <div class="modal-footer">
-            <button type="button" class="btn btn-primary" data-dismiss="modal" onClick="location.href=window.location.pathname"><?php echo _("Close")?></button>
+            <button type="button" class="btn btn-primary" data-dismiss="modal" onClick="location.href='<?PHP echo $modifyURL; ?>'"><?php echo _("Close")?></button>
          </div>
       </div>
    </div>
@@ -56,8 +60,7 @@ background-color: #ffa500;
 $('#myModal').modal('show');
 </script>
 <?php
-}
-
+} //Ending of alert function
 
 //ndex page
 if (basename($_SERVER['PHP_SELF'], '.php') == "index") {
@@ -101,4 +104,16 @@ if (basename($_SERVER['PHP_SELF'], '.php') == "register") {
         echo alert('error', 'Email Address Already Exists', 'The email address already exists. Please login or contact the support team in order to reset your password.');
     }
 }
+
+//Modifying URL for when the Alert is closed to remove the alert paramter from it.
+function modify_url( $url, $param ) {
+    $base_url = strtok($url, '?');              // Get the base url
+    $parsed_url = parse_url($url);              // Parse it
+    $query = $parsed_url['query'];              // Get the query string
+    parse_str( $query, $parameters );           // Convert Parameters into array
+    unset( $parameters[$param] );               // Delete the one you want
+    $new_query = http_build_query($parameters); // Rebuilt query string
+    return $base_url.'?'.$new_query;            // Final modified URL
+}
+
 ?>
