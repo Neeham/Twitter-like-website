8f2e76b225a84ab9d8f18a3a2a88fa2601958e15
Neeham Khalid <Neehamk@gmail.com>
2019-02-20 22:32:05 -0500

    A user can be searched in order to visit their profile.

--- repeated/header.php
+++ repeated/header.php
@@ -12,7 +12,7 @@
   <meta name="msapplication-config" content="https://haxstar.com/images/favicon/browserconfig.xml?v=1.1">
   <meta name="theme-color" content="#ffffff">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-  <link rel="stylesheet" type="text/css" href="https://haxstar.com/css/custom.css?v=1.2">
+  <link rel="stylesheet" type="text/css" href="https://haxstar.com/css/custom.css?v=1.5">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"
     integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
@@ -20,6 +20,6 @@
           crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
   <?php require $_SERVER['DOCUMENT_ROOT'] . '/assets/alert.php';?>
 </head>
