64574496c018257d39cc6392b7ffea1aeba9a4a5
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-23 19:44:05 -0500

    Issue #124 - Testing assertTrue method from PHPUnit_Framework

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -4,7 +4,7 @@ class SampleTest extends \PHPUnit_Framework_TestCase
 {
   public function testSampleForBasicSetup()
   {
-    //
+    $this->assertTrue(true); //checks with assetsTrue to see if param is true 
   }
 }
 
