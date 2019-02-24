5178dfcda004e8555076a8e4782b2c807071b35f
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-23 20:22:17 -0500

    Issue #124 - Testing test cases connection

--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -2,11 +2,38 @@
 
 class SampleTest extends \PHPUnit_Framework_TestCase
 {
-  public function testSampleForBasicSetup()
+  public function testObjectTestConnection()
   {
-    $this->assertTrue(true); //checks with assetsTrue to see if param is true 
+    //$this->assertTrue(true); //checks with assetsTrue to see if param is true
+
+    $object = new \tests\ObjectTest;
+
+    $object->getInputText();
+
+    $this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
   }
 }
+
+
+
+
+/*Notes
+
+Referencing this link for tutorial on tests - https://www.youtube.com/watch?v=V3xrGsUIYis
+
+SampleTest.php outline
+
+$object = new class with methods like getInputText()
+
+$object->getInputText();
+
+$this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
+
+
+
+
+*/
+
 
 
 ?>
