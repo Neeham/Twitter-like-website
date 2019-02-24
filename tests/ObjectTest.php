03482b5acc1c1291327f738e37b81da5a128e70c
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-23 20:50:15 -0500

    Issue #124 - Stopping for the night, saving last minutes notes for tomorrow

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -1,12 +1,17 @@
 <?php
-
 namespace tests;
+require $_SERVER['DOCUMENT_ROOT'] . '/pages/feed.php';
 
 class ObjectTest
 {
   public function getInputText()
   {
-    return 'Hiii soo not null!'; 
+    return 'Hii testing getInputText';
+  }
+
+  public function setUsernameOfLoggedInUser($givenUsernameFromQuery)
+  {
+
   }
 }
 
