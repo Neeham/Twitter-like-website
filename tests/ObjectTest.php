77cc39279d1da486842aba4ddec5134a42d334a2
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-24 15:35:36 -0500

    Issue #124 - Erased variables to see Travis response

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -2,25 +2,19 @@
 namespace tests;
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
 
-$localVariable = 'Hello from outside the class!';
-echo $localVariable;
-
 class ObjectTest
 {
-
-  $localVariable = 'Hello from the class!';
-  echo $localVariable;
+  //public $localVariable;
 
   public function getUsernameOfLoggedInUser()
   {
-    $localVariable = 'Hello from the get!';
-    return $localVariable;
+    return 'Hi from get';
   }
 
   public function setUsernameOfLoggedInUser($givenUsernameFromTextarea)
   {
-      $localVariable = 'Hello from the set!';
-      $this->$localVariable = $givenUsernameFromTextarea;
+      //$this->$localVariable = $givenUsernameFromTextarea;
+      return 'Hi from set';
 
   }
 }
