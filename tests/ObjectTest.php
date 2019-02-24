1e8e2c68578cc51fbbda008e67327bbf31bb29ca
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-24 15:48:29 -0500

    Issue #124 - Variable identified, checking compatibility with methods

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -1,4 +1,7 @@
 <?php
+
+//This is the page that Travis CI reference for success or fail
+
 namespace tests;
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
 
@@ -8,19 +11,19 @@ class ObjectTest
 
   public function getUsernameOfLoggedInUser()
   {
-    return 'Hi from get';
+    return $this->$localVariable;
   }
 
   public function setUsernameOfLoggedInUser($givenUsernameFromTextarea)
   {
-      //$this->$localVariable = $givenUsernameFromTextarea;
-      return 'Hi from set';
+      $this->$localVariable = $givenUsernameFromTextarea;
+      return $this->$localVariable;
 
   }
 
   public function __toString()
   {
-    return 'Testing toString';
+    return $this->$localVariable;
   }
 }
 
