c15358711cdca22a0f97ccdc50878a6a85f5b76a
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-24 20:21:09 -0500

    Issue #124 - deleted everything to test response (previous work is saved locally though)

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -2,34 +2,10 @@
 
 //This is the page that Travis CI reference for success or fail
 
-namespace tests;
 
 class ObjectTest
-{                              /*
-  //global variable used to get/set methods
-  //rotected $localVariable = '';
+{                              
 
-  public function getUsernameOfLoggedInUser()
-  {
-    //global is required in order to associate with class $localVariable
-    //global $localVariable;
-
-    return $this->localVariable;
-  }
-
-  public function setUsernameOfLoggedInUser($givenUsernameFromTextarea)
-  {
-    //  global $localVariable;
-      $this->localVariable = $givenUsernameFromTextarea;
-      return $this->localVariable;
-
-  }
-
-  public function __toString()
-  {
-//    global $localVariable;
-    return $this->localVariable;
-  }                                                  */
 }
 
  ?>
--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -1,39 +1,8 @@
 <?php
-//require(dirname(__FILE__)."/../assets/feed.php");
-//echo $testingGetVariable;
-
-//require "vendor/autoload.php";
 
 class SampleTest extends \PHPUnit_Framework_TestCase
 {
-  $this->assertTrue(true);
-/*
-  public function SampleTest()
-  {
-    global $object;
-    $this->object = new \tests\ObjectTest;
-  }
 
-  public function testGetTheContentFromFeed()
-  {
-    //$this->assertTrue(true); //checks with assetsTrue to see if param is true
-
-     $object = new \tests\ObjectTest;
-
-     $object->setUsernameOfLoggedInUser('Updated');
-
-     echo "setUsernameofLoggedInUser: ".$object;
-
-     $object->getUsernameOfLoggedInUser();
-
-     echo "getUsernameofLoggedInUser: ".$object;
-
-     $object->setUsernameOfLoggedInUser('Updated Again');
-
-     echo "setUsernameofLoggedInUser: ".$object;
-
-     $this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
-  } */
 }
 
 
