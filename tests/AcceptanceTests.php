5b2504c40b1c9f92ee0815c89736e55582d6ccaa
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-24 21:30:29 -0500

    Issue #124 - Trying to see if Travis will notice error in files

---
+++ tests/AcceptanceTests.php
@@ -0,0 +1,21 @@
+<?php
+
+class AcceptanceTests extends \PHPUnit_Framework_TestCase
+{
+  $this->assertTrue(true);
+
+  $object = new \tests\ObjectTest;
+
+  $object->getUsernameOfLoggedInUser();
+
+  echo "getUsernameofLoggedInUser: ".$object;
+
+  $object->setUsernameOfLoggedInUser('Testing 101');
+
+  echo "setUsernameofLoggedInUser: " + $object;
+  
+  echo "setUsernameofLoggedInUser: ".$object;
+}
+
+
+?>
