d9f1b670aff78c46c4365c2d0ca09f4a32d67673
Amanda Kolopanis <amandakolopa@gmail.com>
2019-02-24 23:12:40 -0500

    Issue #124 - Final attempt for the night: revert all files to when Travis was performing test executions

---
+++ tests/SampleTest.php
@@ -0,0 +1,22 @@
+<?php
+class SampleTest extends \PHPUnit_Framework_TestCase
+{
+  public function testToGetTheTextareaContent()
+  {
+    //$this->assertTrue(true); //checks with assetsTrue to see if param is true
+    $object = new \tests\ObjectTest;
+    $object->getUsernameOfLoggedInUser();
+    echo "getUsernameofLoggedInUser: ".$object;
+    $object->setUsernameOfLoggedInUser('Testing 101');
+    echo "setUsernameofLoggedInUser: ".$object;
+    $this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
+  }
+}
+/*Notes
+Referencing this link for tutorial on tests - https://www.youtube.com/watch?v=V3xrGsUIYis
+SampleTest.php outline
+$object = new class with methods like getInputText()
+$object->getInputText();
+$this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
+*/
+?>
