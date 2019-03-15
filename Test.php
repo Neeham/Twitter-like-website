13ea638c4ae79e52826135d8711e96e6a48a2009
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-15 18:48:40 -0400

    Issue #124 - Altering the Travis implementation as an attempt to simplify it

---
+++ Test.php
@@ -0,0 +1,18 @@
+<?php
+
+// backward compatibility
+if (!class_exists('\PHPUnit\Framework\TestCase'))
+{
+    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+}
+
+//include 'index.php';
+
+Class Test extends PHPUnit_Framework_TestCase
+{
+	public function basicTest()
+  {
+		$this->assertTrue(true);
+    echo "Made it into the Test.php file :D";
+	}
+}
