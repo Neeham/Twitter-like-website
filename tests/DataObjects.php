87e33b4b93ede54800535e786f606602697b129c
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-15 19:45:30 -0400

    Issue #124 - Attempting to require query.php with php DIR

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -1,6 +1,6 @@
 <?php
 
-include '../assets/query.php';
+require __DIR__ . "/assets/query.php"
 
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
 class DataObjects
