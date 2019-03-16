a61df6b3d6d43ba637318f238368ea8f3d2efec1
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-16 09:18:25 -0400

    Issue #124 - Moved require config.php and query.php to function

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -1,6 +1,4 @@
 <?php
-require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
-require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
 
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
 class DataObjects
@@ -10,6 +8,8 @@ class DataObjects
   public function getUsernameOfLoggedInUser()
   {
     require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
+    require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
+
     global $localVariable;
 
     $localVariable = (string)$loggedInUser;
