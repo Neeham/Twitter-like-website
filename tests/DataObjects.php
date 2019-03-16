0274629a9a5e67cae27a6b5d5f3a9c8633ea0b56
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-15 20:12:24 -0400

    Issue #124 - require is successful, attempting to fix fatal error with including config.php

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -1,11 +1,12 @@
 <?php
-
+require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
 require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
 
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
 class DataObjects
 {
   protected $localVariable = '';
+  
   public function getUsernameOfLoggedInUser()
   {
     global $localVariable;
