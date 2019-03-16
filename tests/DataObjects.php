683cf748f8f973cb330a15fb565c510102130597
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-15 20:04:59 -0400

    Issue #124 - Attempting to use absolute path for require line

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -1,6 +1,6 @@
 <?php
 
-require "../../assets/query.php";
+require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
 
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
 class DataObjects
