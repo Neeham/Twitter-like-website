223b504b1989a283addaf5e3fe868d9e88ee9fe1
Neeham Khalid <Neehamk@gmail.com>
2019-03-18 13:23:58 -0400

    Updating file based on the master version to test travis.

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -1,20 +1,17 @@
 <?php
-//This is the page that Travis CI reference for success or fail
-namespace tests;
+require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
+require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
 //include 'http://www.haxstar.com/pages/feed.php';  //worked but gave warning for not opening, need a better solution to link to feed without server
-class ObjectTest
+class DataObjects
 {
   protected $localVariable = '';
   public function getUsernameOfLoggedInUser()
   {
+    require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
+    require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
     global $localVariable;
+    $localVariable = (string)$loggedInUser;
     return $this->localVariable;
-  }
-  public function setUsernameOfLoggedInUser($givenUsernameFromTextarea)
-  {
-      global $localVariable;
-      $this->localVariable = $givenUsernameFromTextarea;
-      return $this->localVariable;
   }
   public function __toString()
   {
@@ -22,4 +19,5 @@ class ObjectTest
     return $this->localVariable;
   }
 }
- ?>
+mysqli_close($conn);
+?>
--- tests/SampleTest.php
+++ tests/SampleTest.php
@@ -1,36 +1,46 @@
 <?php
-class SampleTest extends \PHPUnit_Framework_TestCase
+include 'DataObjects.php';
+require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
+require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
+class TestCases extends \PHPUnit_Framework_TestCase
 {
-  public function testToGetTheTextareaContent()
+  public function testToCheckIfAUserIsLoggedIn()
   {
-    //$this->assertTrue(true); //checks with assetsTrue to see if param is true
-    $object = new \tests\ObjectTest;
-
-    $object->getUsernameOfLoggedInUser();
-
-    echo "getUsernameofLoggedInUser: ".$object;
-
-    $object->setUsernameOfLoggedInUser('Checking to see if parameters are passing to ObjectTest');
-
-    echo "setUsernameofLoggedInUser: ".$object;
-
-    $this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
+    require "/home/travis/build/Neeham/Twitter-like-website/assets/config.php";
+    require "/home/travis/build/Neeham/Twitter-like-website/assets/query.php";
+    $this->assertTrue(true); //TESTING - checks with assetsTrue to see if param is true
+    $getLoggedInUser = new DataObjects;
+    $getLoggedInUser->getUsernameOfLoggedInUser();
+    echo "getUsernameofLoggedInUser: ".$getLoggedInUser;
+    $this->assertNotNull($getLoggedInUser);    //will check if the input text is empty or not (this will display through Travis CI )
+  }
+  //core feature: post a Quack
+  public function testToCheckIfASubmittedQuackIsEmpty()
+  {
+  }
+  //core feature: follow a user
+  public function test()
+  {
+  }
+  //core feature: like a Quack
+  public function testToCheckIfCountReturnsResult()
+  {
+    //call function countLikes($getFromDataObjects)
+    //modify countLikes to return the number of likes
+    //check if the return value is an integer number
+  }
+  //additional feature: search for a user
+  public function testToCheckIfTheSearchIsEmpty()
+  {
   }
 }
-
 /*Notes
-
 Referencing this link for tutorial on tests - https://www.youtube.com/watch?v=V3xrGsUIYis
-
-
 SampleTest.php outline
-
 --> Future: see if you can associate pages like profile and feed to a namespace in order to access data
-
-
-
 $object = new class with methods like getInputText()
 $object->getInputText();
 $this->assertNotNull($object);    //will check if the input text is empty or not (this will display through Travis CI )
 */
+mysqli_close($conn);
 ?>
