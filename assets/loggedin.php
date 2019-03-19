baae02bfa438f168c337246333f6768b3b692078
Neeham Khalid <Neehamk@gmail.com>
2019-03-19 09:47:52 -0400

    Made changes to how sessions and cookies are handled.

--- assets/loggedin.php
+++ assets/loggedin.php
@@ -1,15 +1,17 @@
 <?php
 session_start();
-if (!isset($_COOKIE["cookieID"]) && !isset($_SESSION["sessionID"])) { // Cookie and session not set, redirect to index
+if (isset($_COOKIE["cookieID"])) {
+  $_SESSION["sessionID"]               = $_COOKIE["cookieID"];
+  $_SESSION["sessionUsername"]         = $_COOKIE["cookieUsername"];
+  $_SESSION["sessionActivated"]        = $_COOKIE["cookieActivated"];
+  $_SESSION["sessionLastLoggedIn"]     = $_COOKIE["cookieLoggedIn"];
+}
+
+if ((isset($_SESSION["sessionID"])) && ($_SESSION['sessionActivated'] == "0")) {
+  header('Location: https://www.haxstar.com/?Alert=verifyEmail');
+  exit();
+} else if (!isset($_SESSION["sessionID"])) {
   header('Location: https://www.haxstar.com');
   exit();
-} else if ((isset($_COOKIE["cookieID"]) && ($_COOKIE["cookieActivated"] == "0")) || ((isset($_SESSION["sessionID"])) && ($_SESSION['sessionActivated'] == "0"))) { //Cookie or session is set but account not activated, redirect to index
-    header('Location: https://www.haxstar.com/?Alert=verifyEmail');
-    exit();
-} else if (isset($_COOKIE["cookieID"]) && isset($_COOKIE["cookieUsername"]) && isset($_COOKIE["cookieActivated"]) && ($_COOKIE["cookieActivated"] == "1") && isset($_COOKIE["cookieLoggedIn"])) { //If cookie is set and account is activated, set session.
-  $_SESSION["sessionID"]        = $_COOKIE["cookieID"];
-  $_SESSION["sessionUsername"]  = $_COOKIE["cookieUsername"];
-  $_SESSION["sessionActivated"] = $_COOKIE["cookieActivated"];
-  $_SESSION["sessionLastLoggedIn"]     = $_COOKIE["cookieLoggedIn"];
 }
 ?>
