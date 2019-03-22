19c7ce035dfc671371abfb8338bbc1f154270099
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-22 13:21:42 -0400

    Issue #124 - Test was successful, checking if boolean values are accepted as well

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -9,12 +9,12 @@ class DataObjects
     //check if first and last names do not contain numbers
     if(ctype_alpha($firstName) && ctype_alpha($lastName) && is_string($username) && is_string($password) && is_string($email))
     {
-      $this->variableToCheck = 'true';
+      $this->variableToCheck = true;
       return $this->variableToCheck;
     }
     echo 'Firstname: '.gettype($firstName).'    Lastname: '.gettype($lastName).'    Username: '.gettype($username).'    Password: '.gettype($password).'    Email: '.gettype($email);
 
-    $this->variableToCheck = 'false';
+    $this->variableToCheck = false;
     return $this->variableToCheck;
   }
 
