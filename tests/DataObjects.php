441ea906f1edc3bb347944805d4a1f65018786f0
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-22 13:19:18 -0400

    Issue #124 - Incorporated $this-> to class variable to see if reference is working

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -2,19 +2,19 @@
 
 class DataObjects
 {
-  public $variableToCheck = '';
+  public $variableToCheck;
 
   public function testingRegistrationInput($firstName, $lastName, $username, $password, $email)
   {
     //check if first and last names do not contain numbers
     if(ctype_alpha($firstName) && ctype_alpha($lastName) && is_string($username) && is_string($password) && is_string($email))
     {
-      $variableToCheck = 'true';
+      $this->variableToCheck = 'true';
       return $this->variableToCheck;
     }
     echo 'Firstname: '.gettype($firstName).'    Lastname: '.gettype($lastName).'    Username: '.gettype($username).'    Password: '.gettype($password).'    Email: '.gettype($email);
 
-    $variableToCheck = 'false';
+    $this->variableToCheck = 'false';
     return $this->variableToCheck;
   }
 
