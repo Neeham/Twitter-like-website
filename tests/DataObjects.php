1507ae81733064cd5deeabd1f5dd2d4e464297c7
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-22 12:58:48 -0400

    Issue #124 - Added a global variable to DataObjects for toString to function

--- tests/DataObjects.php
+++ tests/DataObjects.php
@@ -2,21 +2,26 @@
 
 class DataObjects
 {
+
+  global $variableToCheck;
+
   public function testingRegistrationInput($firstName, $lastName, $username, $password, $email)
   {
     //check if first and last names do not contain numbers
     if(ctype_alpha($firstName) && ctype_alpha($lastName) && is_string($username) && is_string($password) && is_string($email))
     {
-      return 'Good';
+      $variableToCheck = true;
+      return $this->variableToCheck;
     }
     echo 'Firstname: '.gettype($firstName).'    Lastname: '.gettype($lastName).'    Username: '.gettype($username).'    Password: '.gettype($password).'    Email: '.gettype($email);
 
-    return null;
+    $variableToCheck = false;
+    return $this->variableToCheck;
   }
 
   public function __toString()
   {
-    return 'toString test';
+    return $this->variableToCheck;
   }
 }
  ?>
