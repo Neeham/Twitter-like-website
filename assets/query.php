8024b129948da1d06066ca8e29d33ddcf3dd1443
Zohal <zohalmir9@gmail.com>
2019-03-24 02:32:48 -0400

    "#102 made the email verification message more appealing, added a button."

--- assets/query.php
+++ assets/query.php
@@ -80,27 +80,115 @@ if (isset($_POST['register'])) {
             $message          = '
 
 
+            <html xmlns="http://www.w3.org/1999/xhtml">
+            <head>
+            	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+              	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
+             	<meta name="format-detection" content="telephone=no"/>
 
-<html>
-<head>
-<title>Welcome to Quacker!</title>
-</head>
-<body>
-<p>Hello ' . $fName . ',</p>
-<table>
-<tr>
-<th>Email Adddress</th>
-<th>Hash</th>
-<th>Activation Link</th>
-</tr>
-<tr>
-<td>' . $email . '</td>
-<td>' . $hash . '</td>
-<td>https://www.haxstar.com/assets/verify?Email=' . $email . '&Hash=' . $hash . '</td>
-</tr>
-</table>
-</body>
-</html>
+            	<style>
+            @media all and (min-width: 560px) {
+            	.container { border-radius: 8px; -webkit-border-radius: 8px; -moz-border-radius: 8px; -khtml-border-radius: 8px;}
+            }
+            </style>
+            	<title>Thank you for signing-up with Quacker!</title>
+            </head>
+
+            <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; width: 100%;" class="background"><tr><td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;"
+            	bgcolor="#2E2E2E">
+
+            <table border="0" cellpadding="0" cellspacing="0" align="center"
+            	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+            	max-width: 560px;" class="wrapper">
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+            			padding-top: 20px;
+            			padding-bottom: 20px;">
+            		</td>
+            	</tr>
+
+            </table>
+
+            <table border="0" cellpadding="0" cellspacing="0" align="center"
+            	bgcolor=" #85D7EB	"
+            	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+            	max-width: 560px;" class="container">
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 24px; font-weight: bold; line-height: 130%;
+            			padding-top: 25px;
+            			color: #000000;
+            			font-family: sans-serif;" class="header">
+            				Email Verification
+            		</td>
+            	</tr>
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0;
+            			padding-top: 20px;" class="hero"><a target="_blank" style="text-decoration: none;"
+            >
+            <img border="0" vspace="0" hspace="0"			src="https://haxstar.com/resources/images/logo/email.png"
+            		title="email Image"
+            		 style="
+            			width: 100%;
+            			max-width: 560px;
+            			color: #000000; font-size: 13px; margin: 0; padding: 0; outline: none; text-decoration: none; -ms-interpolation-mode: bicubic; border: none; display: block;"/></a></td>
+            	</tr>
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%; font-size: 17px; font-weight: 400; line-height: 160%;
+            			padding-top: 25px;
+            			color: #000000;
+            			font-family: sans-serif;" class="paragraph">
+            				Please click on the button below to activate your account.
+            		</td>
+            	</tr>
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+            			padding-top: 25px;
+            			padding-bottom: 5px;" class="button"><a
+            			href="https://github.com/konsav/email-templates/" target="_blank" style="text-decoration: underline;">
+            				<table border="0" cellpadding="0" cellspacing="0" align="center" style="max-width: 240px; min-width: 120px; border-collapse: collapse; border-spacing: 0; padding: 0;"><tr><td align="center" valign="middle" style="padding: 12px 24px; margin: 0; text-decoration: underline; border-collapse: collapse; border-spacing: 0; border-radius: 4px; -webkit-border-radius: 4px; -moz-border-radius: 4px; -khtml-border-radius: 4px;"
+            					bgcolor="#E9703E"><a target="_blank" style="text-decoration: underline;
+            					color: #FFFFFF; font-family: sans-serif; font-size: 17px; font-weight: 400; line-height: 120%;"
+            					href="https://www.haxstar.com/assets/verify?Email=' . $email . '&Hash=' . $hash . '">
+            						Activate Account
+            					</a>
+            			</td></tr></table></a>
+            		</td>
+            	</tr>
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+            			padding-top: 25px;" class="line"><hr
+            			color="#E0E0E0" align="center" width="100%" size="1" noshade style="margin: 0; padding: 0;" />
+            		</td>
+            	</tr>
+
+            </table>
+
+            <table border="0" cellpadding="0" cellspacing="0" align="center"
+            	width="560" style="border-collapse: collapse; border-spacing: 0; padding: 0; width: inherit;
+            	max-width: 560px;" class="wrapper">
+
+            	<tr>
+            		<td align="center" valign="top" style="border-collapse: collapse; border-spacing: 0; margin: 0; padding: 0; padding-left: 6.25%; padding-right: 6.25%; width: 87.5%;
+            			padding-top: 25px;" class="social-icons">
+            			<table
+            			width="256" border="0" cellpadding="0" cellspacing="0" align="center" style="border-collapse: collapse; border-spacing: 0; padding: 0;">
+            			</table>
+            		</td>
+            	</tr>
+
+
+            </table>
+
+            </td></tr></table>
+
+            </body>
+            </html>
 
 
 
