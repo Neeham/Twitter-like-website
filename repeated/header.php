126e1cedd8271190154ded794ede40d1acfd0cf6
“Your <loujain_alnakeeb@hotmail.com>
2019-03-24 18:37:08 -0400

    Error found in previous code for the version in the header

--- repeated/header.php
+++ repeated/header.php
@@ -33,7 +33,7 @@
    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css?v=8.0"
       integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
    <!-- JS -->
-   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js?v=8.1"></script>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js?v=8.0"></script>
    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js?=8.0"
       integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js?=8.0"
