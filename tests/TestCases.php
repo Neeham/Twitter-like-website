5d2e35522e28a094e25b502dcd188b168e3ab145
Amanda Kolopanis <amandakolopa@gmail.com>
2019-03-27 22:21:00 -0400

    Issue #103 - Refactored DataObjects and TestCases (added comments)

--- tests/TestCases.php
+++ tests/TestCases.php
@@ -8,16 +8,16 @@ class TestCases extends \PHPUnit_Framework_TestCase
     $successfulRegistrationValues = new DataObjects;
     $failureRegistrationValues = new DataObjects;
 
+    //Successful Input
     $successfulRegistrationValues->register('A', 'A', 'A', 'A', 'A@A.com');
-    //echo "Successful: ".$successfulRegistrationValues;
     $this->assertEquals($successfulRegistrationValues, 'true');
 
+    //Failure Input: invalid email address
     $failureRegistrationValues->register('1234', '5678', 'A', 'A', 'A@.com');
-    //echo "  Failure: ".$failureRegistrationValues;
     $this->assertEquals($failureRegistrationValues, 'false');
 
+    //Failure Input: first and last name are int
     $failureRegistrationValues->register(1234, 5678, 'A', 'A', 'A@.com');
-    //echo "  Failure: ".$failureRegistrationValues;
     $this->assertEquals($failureRegistrationValues, 'false');
   }
 
@@ -27,21 +27,19 @@ class TestCases extends \PHPUnit_Framework_TestCase
     $successfulQuackPost = new DataObjects;
     $failureQuackPost = new DataObjects;
 
+    //Successful Input
     $successfulQuackPost->postQuack(1, 'Hello World', '2019-03-18 22:28:05');
-    //echo "Successful: ".$successfulQuackPost;
     $this->assertEquals($successfulQuackPost, 'true');
 
-    //Input a Quack with an invalid date
+    //Failure Input: Quack with an invalid date
     $failureQuackPost->postQuack(1, 'Hello World', '2019-13-17 22:28:05');
-    //echo "Failure: ".$failureQuackPost;
     $this->assertEquals($failureQuackPost, 'false');
 
-    //Input an empty Quack
+    //Failure Input: an empty Quack
     $failureQuackPost->postQuack(1, '', '2019-03-17 22:28:05');
-    //echo "Failure: ".$failureQuackPost;
     $this->assertEquals($failureQuackPost, 'false');
 
-    //Input a Quack with a character length of 256
+    //Failure Input: a Quack with a character length of 256
     $failureQuackPost->postQuack(1, 'g7cAs43CRVmyWIe16akYnVjVIXXF5QFNb8PKmZoswj8geGD849myKiNR31PW4g5Ho5at0ErYepAU2SiH92INITGDOGqZ31F390YYHaUr6FLiwGFBQQNybaI9V44G56pyr9EcykgHCGtMOtcGnyLf0RNV4C16W31Rkottj7aP1x4JvPT77NI6OaaJLlsE31wKB3pgLne4H8VWG5GGu4Y4gAB9XShWcOUh3qHUkuJzC0lf0QpW3V7bYDaKLAANFeWb', '2019-03-13 22:28:05');
     $this->assertEquals($failureQuackPost, 'false');
   }
@@ -52,12 +50,12 @@ class TestCases extends \PHPUnit_Framework_TestCase
     $successfulFollowUser = new DataObjects;
     $failureFollowUser = new DataObjects;
 
+    //Successful Input
     $successfulFollowUser->followUser(1, 5);
-    //echo "Successful: ".$successfulQuackPost;
     $this->assertEquals($successfulFollowUser, 'true');
 
+    //Failure Input: user IDs are strings
     $failureFollowUser->followUser('1', '5');
-    //echo "Successful: ".$successfulQuackPost;
     $this->assertEquals($failureFollowUser, 'false');
 
   }
@@ -68,40 +66,35 @@ class TestCases extends \PHPUnit_Framework_TestCase
     $successfulLikeQuack = new DataObjects;
     $failureLikeQuack = new DataObjects;
 
+    //Successful Input
     $successfulLikeQuack->likeQuack(10, 1, '2019-03-18 22:28:05');
-    //echo "Successful: ".$successfulQuackPost;
     $this->assertEquals($successfulLikeQuack, 'true');
 
-    //Liking a Quack with an invalid date
+    //Failure Input: liking a Quack with an invalid date
     $failureLikeQuack->likeQuack(10, 1, '2019-44-18 22:28:05');
-    //echo "Successful: ".$successfulQuackPost;
     $this->assertEquals($failureLikeQuack, 'false');
 
-    //Liking a Quack with invalid input for IDs (strings not ints)
+    //Failure Input: liking a Quack with user ID and tweet ID as strings
     $failureLikeQuack->likeQuack('10', '1', '2019-03-18 22:28:05');
-    //echo "Successful: ".$successfulQuackPost;
     $this->assertEquals($failureLikeQuack, 'false');
   }
 
   // ####################################### Test Case - Search a User #######################################
   public function testTheSearchInputIsEmpty()
   {
-      //check if the input is a string and not empty
       $successfulSearchUser = new DataObjects;
       $failureSearchUser = new DataObjects;
 
+      //Successful Input
       $successfulSearchUser->searchUser('Oussama');
-      //echo "Successful: ".$successfulQuackPost;
       $this->assertEquals($successfulSearchUser, 'true');
 
-      //Inputting an empty search
+      //Failure Input: search with an empty value
       $failureSearchUser->searchUser('');
-      //echo "Successful: ".$successfulQuackPost;
       $this->assertEquals($failureSearchUser, 'false');
 
-      //Inputting an invalid username
+      //Failure Input: search a username as an int
       $failureSearchUser->searchUser(23);
-      //echo "Successful: ".$successfulQuackPost;
       $this->assertEquals($failureSearchUser, 'false');
   }
 }
